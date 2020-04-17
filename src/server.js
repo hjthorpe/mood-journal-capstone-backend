89cd3e9d89e5e38da215fbfdc7d1480eca68394a
Hannah Thorpe <hthorpe@protonmail.com>
2020-04-17 11:47:10 -0400

    simple server running

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
-const app = require('./app');
+// const app = require('./app');
 
-const { PORT } = require('./config');
+// const { PORT } = require('./config');
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
+
+module.exports = {app};
