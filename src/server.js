d08615563283dc167db7a3e8cd0f43b454d1e931
Hannah Thorpe <hthorpe@protonmail.com>
2020-04-17 20:17:57 -0400

    added cors middleware

--- src/server.js
+++ src/server.js
@@ -4,13 +4,21 @@
 // ** BEGIN MOOD-JOURNAL-CAPSTONE SERVER ** //
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const {CLIENT_ORIGIN} = require('./config');)
 
 app.get('/api/*', (req, res) => {
   res.json({ok: true});
 });
+
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
