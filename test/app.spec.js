0356396eeab2cd7ec2c5b095e11d7ba63d4e96ba
Hannah Thorpe <hthorpe@protonmail.com>
2020-04-23 14:07:13 -0400

    final commit

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,11 +1,40 @@
 const { expect } = require('chai');
 const supertest = require('supertest');
-const server = require('../src/server');
+const { app } = require('../src/server');
 
+
+//GET ENDPOINT
 describe('GET endpoint', () => {
-  it('GET / responds with 200 containing moodjournalentries contents', () => {
-    return supertest(server)
+  it('GET / responds with 200', () => {
+    return supertest(app)
       .get('/api/moodjournal/entries')
       .expect(200);
+  });
+});
+
+//POST ENDPOINT
+describe('POST endpoint', () => {
+  it('POST / responds with 201', () => {
+    return supertest(app)
+      .post('/api/moodjournal/entries/post')
+      .expect(201);
+  });
+});
+
+//PATCH ENDPOINT
+describe('PATCH endpoint', () => {
+  it('PATCH / responds with 201', () => {
+    return supertest(app)
+      .patch('/api/moodjournal/entries/17')
+      .expect(201);
+  });
+});
+
+//DELETE ENDPOINT
+describe('DELETE endpoint', () => {
+  it('DELETE / responds with 204', () => {
+    return supertest(app)
+      .delete('/api/moodjournal/entries/17')
+      .expect(204);
   });
 });
