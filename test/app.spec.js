e31472417c140c0b813f6a7547b8e5021116a66e
Hannah Thorpe <hthorpe@protonmail.com>
2020-04-23 11:40:42 -0400

    help with testing

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,10 +1,11 @@
+const { expect } = require('chai');
+const supertest = require('supertest');
+const server = require('../src/server');
 
-const app = require('../src/app');
-
-describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
-    return supertest(app)
-      .get('/')
-      .expect(200, 'Hello, world!');
+describe('GET endpoint', () => {
+  it('GET / responds with 200 containing moodjournalentries contents', () => {
+    return supertest(server)
+      .get('/api/moodjournal/entries')
+      .expect(200);
   });
 });
