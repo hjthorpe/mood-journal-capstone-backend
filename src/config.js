8e0c1f27e03b12a9551ff8b62866c918dc98b4c7
Hannah Thorpe <hthorpe@protonmail.com>
2020-04-24 11:16:07 -0400

    migrated tables and updated connectionString

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const { Pool, Client } = require('pg');
 const isProduction = process.env.NODE_ENV === 'production';
 
 const connectionString = 
-'postgressql://postgres:Antolini1@localhost:5432/mood-journal-capstone';
+'postgres://ciwpdhyfvebvqf:67d448cfbf7562f03873ec3ba8b91526fa7c0b9443f79bab8ad1d9c162820652@ec2-23-20-129-146.compute-1.amazonaws.com:5432/d60uf9sb83didd';
 
 const client = new Client({
   connectionString:connectionString
