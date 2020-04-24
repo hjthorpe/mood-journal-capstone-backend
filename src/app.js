454bcc8b937fbf65ee72109730b96d2c6ff30084
Hannah Thorpe <hthorpe@protonmail.com>
2020-04-24 13:29:25 -0400

    changed cors(); line

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 
 const app = express();
+app.use(cors());
 const { NODE_ENV } = require('./config');
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
@@ -12,7 +13,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+
 
 
 
